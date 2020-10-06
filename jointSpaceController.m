2b577234e39303c13d37f3278b20b26e2cf0a476
Leonardo Brizi <leobrizi.96@gmail.com>
2020-10-06 18:03:54 +0200

    kinematic contorl of joint motion

--- jointSpaceController.m
+++ jointSpaceController.m
@@ -1,8 +1,6 @@
 function [qd_new, q_control_dot] = jointSpaceController(qd, xd, q, Ts, Kp, obs, k0)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% Kinematic control of joint motion
     qd_dot = inverseDifferentialKinematicsAug(qd, xd, obs, k0);
-    %qd_dot = inverseDifferentialKinematics(qd, xd);
     qd_new = qd + qd_dot*Ts;          %Euler integration
     q_err = qd_new - q;
     control_signal = Kp*q_err;
