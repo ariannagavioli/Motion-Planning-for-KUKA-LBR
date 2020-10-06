c7c521bff03f9fde7a396ac031536a06a3a226e6
ariannagavioli <gavioli.arianna@gmail.com>
2020-10-06 18:24:23 +0200

    standard inverse differential kinematics

--- inverseDifferentialKinematics.m
+++ inverseDifferentialKinematics.m
@@ -1,6 +1,4 @@
 function qd = inverseDifferentialKinematics(q, xd)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
     J = getAnaliticJacobian(q);
     J_pinv = pinv(J);
     qd = J_pinv * xd;
